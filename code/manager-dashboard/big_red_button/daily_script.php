5b1a37a003ada405db53ddd7102a824aa2e01ea8
Nazmul Hossain <nazmulhossain@iut-dhaka.edu>
2023-05-31 20:19:37 +0600

    notification update

--- code/manager-dashboard/big_red_button/daily_script.php
+++ code/manager-dashboard/big_red_button/daily_script.php
@@ -16,12 +16,13 @@ foreach($query1 as $res){
     $query_run3 = mysqli_query($con, $sql3);
     $desc = "Some Inventory Items will expire within 7 days";
     if(mysqli_num_rows($query_run3)>0){
-        $res3 = mysqli_fetch_array($query_run3);
-        if($res3['n_status']===1){
-            $sql4 = "UPDATE notifications SET n_reason = '$desc', n_status = 0 WHERE n_desc = '$in_name'";
-            $query_run4 = mysqli_query($con, $sql4);
-            echo "About to expire Update ".$in_name;
-        }
+        // $res3 = mysqli_fetch_array($query_run3);
+        // if($res3['n_status']===1){
+        //     $sql4 = "UPDATE notifications SET n_reason = '$desc', n_status = 0 WHERE n_desc = '$in_name'";
+        //     $query_run4 = mysqli_query($con, $sql4);
+        //     echo "About to expire Update ".$in_name;
+        // }
+        ;
     }
     else{
         $sql5 = "INSERT INTO `notifications`(`n_id`, `n_desc`,`n_reason`) VALUES ('default','$in_name','$desc')";
